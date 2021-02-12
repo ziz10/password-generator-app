4503a059b41a41af42b4004bd3089cc9a3e4e123
Abdoul Bass <malickya8891@gmail.com>
2021-02-11 21:44:33 -0800

    implemented logic for var functionArray

--- script.js
+++ script.js
@@ -28,79 +28,79 @@ function generatePassword() {
   var minimumCount = 0;
 
 
-//   // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
 
-//   var minimumNumbers = "";
-//   var minimumLowerCases = "";
-//   var minimumUpperCases = "";
-//   var minimumSpecialCharacters = "";
-
-
-//   // Generator functions**
-//   var functionArray = {
-//     getNumbers: function() {
-//       return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-//     },
-
-//     getLowerCases: function() {
-//       return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-//     },
-
-//     getUpperCases: function() {
-//       return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-//     },
-
-//     getSpecialCharacters: function() {
-//       return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-//     }
-
-// };
-
-//   // Checks to make sure user selected ok for all and uses empty minimums from above
-
-//   if (numbers === true) {
-//     minimumNumbers = functionArray.getNumbers();
-//     minimumCount++;
-
-//   }
-
-//   if (lowerCases === true) {
-//     minimumLowerCases = functionArray.getLowerCases();
-//     minimumCount++;
-
-//   }
-
-//   if (upperCases === true) {
-//     minimumUpperCases = functionArray.getUpperCases();
-//     minimumCount++;
-
-//   }
-
-//   if (special === true) {
-//     minimumSpecialCharacters = functionArray.getSpecialCharacters();
-//     minimumCount++;
-
-//   }
-
-//   // empty string variable for the for loop below
-//   var randomPasswordGenerated = "";
-
-//   // loop getting random characters
-//   for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-//     var randomNumberPicked = Math.floor(Math.random() * 4);
-
-//     randomPasswordGenerated += randomNumberPicked;
-
-//   }
-
-//   // to make sure characters are added to the password
-//   randomPasswordGenerated += minimumNumbers;
-//   randomPasswordGenerated += minimumLowerCases;
-//   randomPasswordGenerated += minimumUpperCases;
-//   randomPasswordGenerated += minimumSpecialCharacters;
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
 
 
-//   return randomPasswordGenerated;
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
 
-// }
-// generatePassword();
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+
+};
+
+  // Checks to make sure user selected ok for all and uses empty minimums from above
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+
+  }
+
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+
+  // loop getting random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+
+    randomPasswordGenerated += randomNumberPicked;
+
+  }
+
+  // to make sure characters are added to the password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+
+  return randomPasswordGenerated;
+
+}
+generatePassword();
