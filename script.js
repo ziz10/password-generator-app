5b8f69d106381c4c2ea6590ec7768d7da5ff9245
Abdoul Bass <malickya8891@gmail.com>
2021-02-11 21:42:00 -0800

    implemented a generated password function for user input prompts

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ function writePassword() {
 
   passwordText.value = password;
 
-// }
-// writePassword() ;
-// // Prompts that come up after you click generate password
-// function generatePassword() {
-//   var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+}
+writePassword() ;
+// Prompts that come up after you click generate password
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
 
-//   var numbers = confirm("Do you want numbers in your password?");
+  var numbers = confirm("Do you want numbers in your password?");
 
-//   var lowerCases = confirm("Do you want lowercases in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
 
-//   var upperCases = confirm("Do you want uppercases in your password?");
+  var upperCases = confirm("Do you want uppercases in your password?");
 
-//   var special = confirm("Do you want special characters in your password?");
+  var special = confirm("Do you want special characters in your password?");
 
-//   // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
-//   var minimumCount = 0;
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
 
 
 //   // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
